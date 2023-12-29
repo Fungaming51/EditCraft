bf013bbf4d29fcc982f2bb32763892bdaec22182
itsjacob <conyersjacob21@gmail.com>
2023-12-28 21:52:35 -0600

    Update main.py
    changed imports

--- base/main.py
+++ base/main.py
@@ -1,5 +1,8 @@
-from OpenGL.GL import *
-import pygame as pg
-from pygame.locals import *
-from opengl_tools import *
-
+import sys
+import math
+import random
+import time
+from pyglet import image
+from pyglet.gl import *
+from pyglet.graphics import *
+from pyglet.window import key, mouse
