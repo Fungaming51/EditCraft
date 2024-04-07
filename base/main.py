061fe5ad68d41d8aa6a1ef6a24a93983506d0c9d
itsjacob <conyersjacob21@gmail.com>
2024-04-06 23:22:14 -0500

    Update main.py
    idk, but ctypes now

--- base/main.py
+++ base/main.py
@@ -1,18 +1,70 @@
+import ctypes
 import pyglet
 pyglet.options["shadow_window"] = False
 pyglet.options["debug_gl"] = False
 import pyglet.gl as gl
 
+vertex_positions = [ 
+	-0.5,  0.5, 1.0,
+	-0.5, -0.5, 1.0,
+	 0.5, -0.5, 1.0,
+	 0.5,  0.5, 1.0,
+]
+
+indices = [#makes a square (this is for me not for you)
+    0, 1, 2, #first tri
+    0, 2, 3, #second tri
+    ]
+
 class Window(pyglet.window.Window):
     def __init__(self, **args):
         super(Window, self).__init__(**args)
+
+        #vertex array object
+
+        self.vao = gl.GLuint(0)
+        gl.glGenVertexArrays(1, ctypes.byref(self.vao))
+        gl.glBindVertexArray(self.vao)
+
+        #vertex buffer object
+
+        self.vbo = gl.GLuint(0)
+        gl.glGenBuffers(1, ctypes.byref(self.vbo))
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.vbo)
+
+        gl.glBufferData(gl.GL_ARRAY_BUFFER,
+                        ctypes.sizeof(gl.GLfloat * len(vertex_positions)),
+                        (gl.GLfloat * len(vertex_positions)) (*vertex_positions),
+                        gl.GL_STATIC_DRAW)
+
+        gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 0, 0)
+        gl.glEnableVertexAttribArray(0)
+
+        #index buffer object
+
+        self.ibo = gl.GLuint(0)
+
+        gl.glGenBuffers(1, self.ibo)
+        gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, self.ibo)
+
+        gl.glBufferData(gl.GL_ELEMENT_ARRAY_BUFFER,
+                        ctypes.sizeof(gl.GLuint * len(indices)),
+                        (gl.GLuint * len(indices)) (*indices),
+                        gl.GL_STATIC_DRAW)
 
     def onDraw(self):
         gl.glClearColor(1.0, 0.5, 1.0, 1.0)
         self.clear()
 
+        gl.glDrawElements(
+            gl.GL_TRIANGLES,
+            len(indices),
+            gl.GL_UNSIGNED_INT,
+            None)
+
     def onResize(self, width, height):
         print(f"Resize {width} * {height}")  #for testing only
+        gl.glViewport(0, 0, width, hieght)
 
 #the width & height didn't print, changing it not being hard coded didn't work
 #it did something weird though, went off center.
