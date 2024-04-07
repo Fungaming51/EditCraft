ea0404bf4892cc236cc78fd1229d999830e25f13
itsjacob <conyersjacob21@gmail.com>
2024-04-06 22:29:27 -0500

    Update main.py
    window creation

--- base/main.py
+++ base/main.py
@@ -2,3 +2,8 @@ import pyglet
 pyglet.options["shadow_window"] = False
 pyglet.options["debug_gl"] = False
 import pyglet.gl as gl
+
+class Window(pyglet.window.Window):
+    def __init__(self, **args):
+        super(Window, self).__init__(**args)
+        
