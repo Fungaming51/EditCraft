48497b4cab884ece62a0d3c8a8b99a44ef845696
itsjacob <conyersjacob21@gmail.com>
2024-04-06 22:38:55 -0500

    Update main.py
    empty game window

--- base/main.py
+++ base/main.py
@@ -6,4 +6,22 @@ import pyglet.gl as gl
 class Window(pyglet.window.Window):
     def __init__(self, **args):
         super(Window, self).__init__(**args)
-        
+
+    def onDraw(self):
+        gl.glClearColor(1.0, 0.5, 1.0, 1.0)
+        self.clear()
+
+    def onResize(self, width, height):
+        print("resize %d * %d" % (width, height))  #for testing only
+
+class Game:
+    def __init__(self):
+        self.config = gl.Config(major_version = 3)
+        self.window = Window(config = self.config, width = 800, height = 600, caption = "EditCraft", resizable = True, vsync = False)
+
+    def run(self):
+        pyglet.app.run()
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
