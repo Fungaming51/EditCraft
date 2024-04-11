8e8e75553c32e98114b4c14161ae05cf4c1794a6
itsjacob <conyersjacob21@gmail.com>
2024-04-10 19:56:22 -0500

    Update main and shader

--- base/main.py
+++ base/main.py
@@ -1,5 +1,21 @@
 import pyglet
+from pyglet.gl import *
+from pyglet.window import *
 
-window = pyglet.window.Window()
+window = pyglet.window.Window(800,600,"EditCraft")
 
+#controls
+def on_key_press(symbol, modifiers):
+    pass
+
+def on_mouse_press(x, y, button, modifiers):
+    pass
+
+@window.event
+def on_draw():
+   # print("Hello")
+   glBegin(GL_LINES)
+   glVertex3f(100.0,100.0,0.25)
+   glVertex3f(200.0,300.0,-0.75)
+   glEnd()
 pyglet.app.run()
