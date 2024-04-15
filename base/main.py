77f5c527d44eab99d8297d9caa4df9013a441e88
itsjacob <conyersjacob21@gmail.com>
2024-04-15 16:53:19 -0500

    Update main.py

--- base/main.py
+++ base/main.py
@@ -1,21 +1,39 @@
 import pyglet
 from pyglet.gl import *
 from pyglet.window import *
+from pyglet.shapes import *
 
 window = pyglet.window.Window(800,600,"EditCraft")
 
-#controls
-def on_key_press(symbol, modifiers):
-    pass
 
-def on_mouse_press(x, y, button, modifiers):
-    pass
+
+class Player:
+    def __init__(self, pos=(0,0,0), rot=(0,0)):
+        self.pos = list(pos)
+        self.rot = list(rot)
+    def on_key_press():
+        pass
+
+    def on_mouse_press(self, dx, dy):
+        dx/= 8
+        dy/= 8
+        self.rot[0] += dy
+        self.rot[1] -= dx
+        if self.rot[0] > 90:
+            self.rot[0] = 90
+        elif self.rot[0] < -90:
+            self.rot[0] = -90
+
+
 
 @window.event
-def on_draw():
+def on_draw(self):
+    pass
    # print("Hello")
-  pyglet.glBegin(GL_LINES)
-  pyglet.glVertex3f(100.0,100.0,0.25)
-  pyglet.glVertex3f(200.0,300.0,-0.75)
-  pyglet.glEnd()
-pyglet.app.run()
+   #self.clear()
+   #self.set3d()
+   #self.push(self.Player.pos, self.Player.rot)
+   #self.model.draw()
+   #glPopMatrix()
+if __name__ == '__main__':
+    pyglet.app.run()
