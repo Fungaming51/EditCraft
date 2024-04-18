99c20247ce2b706a9d253ed10a69bd3389a1010c
itsjacob <conyersjacob21@gmail.com>
2024-04-18 18:19:02 -0500

    Update

--- base/main.py
+++ base/main.py
@@ -2,38 +2,14 @@ import pyglet
 from pyglet.gl import *
 from pyglet.window import *
 from pyglet.shapes import *
+import shader
 
 window = pyglet.window.Window(800,600,"EditCraft")
-
-
-
-class Player:
-    def __init__(self, pos=(0,0,0), rot=(0,0)):
-        self.pos = list(pos)
-        self.rot = list(rot)
-    def on_key_press():
-        pass
-
-    def on_mouse_press(self, dx, dy):
-        dx/= 8
-        dy/= 8
-        self.rot[0] += dy
-        self.rot[1] -= dx
-        if self.rot[0] > 90:
-            self.rot[0] = 90
-        elif self.rot[0] < -90:
-            self.rot[0] = -90
-
+window.set_location(400, 200)
 
 
 @window.event
-def on_draw(self):
-    pass
-   # print("Hello")
-   #self.clear()
-   #self.set3d()
-   #self.push(self.Player.pos, self.Player.rot)
-   #self.model.draw()
-   #glPopMatrix()
+def on_draw():
+    window.clear()
 if __name__ == '__main__':
     pyglet.app.run()
--- base/shader.py
+++ base/shader.py
@@ -1,29 +1,35 @@
 from pyglet.graphics.shader import Shader, ShaderProgram
+import pyglet
+from pyglet.gl import *
 
-vertex_source = """#version 150 core
-    in vec2 position;
-    in vec4 colors;
-    out vec4 vertex_colors;
+vertex_source = """#version 330
+    layout(location = 0) in vec2 vertices;
+    layout(location = 1) in vec4 colors;
+    out vec4 newColor;
 
     uniform mat4 projection;
 
     void main()
     {
-        gl_Position = projection * vec4(position, 0.0, 1.0);
-        vertex_colors = colors;
+        gl_Position = vec4(vertices, 0.0f, 1.0f);
+        newColor = colors;
     }
 """
 
-fragment_source = """#version 150 core
-    in vec4 vertex_colors;
-    out vec4 final_color;
+fragment_source = """#version 330
+    in vec4 newColor;
+    out vec4 outColor;
 
     void main()
     {
-        final_color = vertex_colors;
+        outColor = newColor;
     }
 """
 
 vert_shader = Shader(vertex_source, 'vertex')
 frag_shader = Shader(fragment_source, 'fragment')
 program = ShaderProgram(vert_shader, frag_shader)
+
+batch = pyglet.graphics.Batch()
+
+program.vertex_list(3, pyglet.gl.GL_TRIANGLES, batch, vertices=('f', (200, 400, 350,300, 450)))
