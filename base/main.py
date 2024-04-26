388f4540ec17256337781d0ac4ea8a7c9eb6b8d3
itsjacob <conyersjacob21@gmail.com>
2024-04-26 15:23:30 -0500

    Add Knife object, Update main

--- base/main.py
+++ base/main.py
@@ -12,7 +12,11 @@ window.set_location(400, 200)
 @window.event
 def on_draw():
     window.clear()
+    batch.draw()
 
 
 if __name__ == '__main__':
+    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_CULL_FACE)
+    model = pyglet.model.load("Knife.obj", batch=batch)
     pyglet.app.run()
