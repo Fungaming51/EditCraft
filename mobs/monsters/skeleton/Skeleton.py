177b8828bb4fa7a02a9039d9b8f19ddcbddb4a7d
itsjacob <conyersjacob21@gmail.com>
2024-06-01 23:10:16 -0500

    location change

--- mobs/monsters/skeleton/Skeleton.py
+++ mobs/monsters/skeleton/Skeleton.py
@@ -4,4 +4,4 @@ from pyglet.gl import *
 batch = pyglet.graphics.Batch()
 
 class skeleton():
-    torso = pyglet.model.load(r"C:\Users\SHANT\Desktop\f\programing\projects\EditCraft\mobs\monsters\skeleton\torso.obj", batch=batch)
+    torso = pyglet.model.load(r"C:\Users\SHANT\Desktop\f\programing\projects\EditCraft\mobs\monsters\skeleton\Skeleton_model\torso.obj", batch=batch)
