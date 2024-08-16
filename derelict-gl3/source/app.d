2dbdc27a8c7c2cdd95072548b4e730060405c77e
itsjacob <conyersjacob21@gmail.com>
2024-08-15 21:00:52 -0500

    I'm going to fucking die
    I'm starting to hate this fucking language

--- derelict-gl3/source/app.d
+++ derelict-gl3/source/app.d
@@ -2,11 +2,5 @@ import std.stdio;
 import derelict.opengl;
 
 void main(){
-	DerelictGL3.load();
-	
-	myCreateContext();
-	
-	DerelictGL3.reload();
-	
 	writeln("Edit source/app.d to start your project.");
 }
