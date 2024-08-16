518548661e477f817586be98db23a9e261b25a95
itsjacob <conyersjacob21@gmail.com>
2024-08-15 20:37:45 -0500

    Update app.d
    it'll be changed, lol

--- derelict-gl3/source/app.d
+++ derelict-gl3/source/app.d
@@ -1,6 +1,12 @@
 import std.stdio;
+import derelict.opengl;
 
-void main()
-{
+void main(){
+	DerelictGL3.load();
+	
+	myCreateContext();
+	
+	DerelictGL3.reload();
+	
 	writeln("Edit source/app.d to start your project.");
 }
