34d17fd26428e1e310fd1cee42cbb03b5b34c94f
itsjacob <conyersjacob21@gmail.com>
2024-08-21 18:51:44 -0500

    add glfw

--- derelict-gl3/source/app.d
+++ derelict-gl3/source/app.d
@@ -1,7 +1,39 @@
 import std.stdio;
-public import derelict.opengl.versions.gl3x;
+import derelict.opengl;
+import derelict.glfw3;
+
 
 void main(){
+	DerelictGLFW3.load();
 	DerelictGL3.load();
-	writeln("Edit source/app.d to start your project.");
+	
+	if (!glfwInit()){
+		writeln("Failed to initialized GLFW...");
+		return;
+	}
+	
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	
+	auto window = glfwCreateWindow(800, 600, "Edit Craft", null, null);
+	
+	if (window is null){
+		writeln("Failed to create GLFW window...");
+		glfwTerminate();
+		return;
+	}
+	
+	glfwMakeContextCurrent(window);
+	DerelictGL3.reload();
+	//main loop
+	while (!glfwWindowShouldClose(window)){
+		glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
 }
