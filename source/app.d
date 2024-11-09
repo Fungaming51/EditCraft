35043eff6d8cc193650af82a9299a1bfd2f60791
itsjacob <conyersjacob21@gmail.com>
2024-11-09 11:53:55 -0600

    Update app.d

--- source/app.d
+++ source/app.d
@@ -1,9 +1,15 @@
-import std.stdio;
-//import derelict.opengl;
-//import derelict.glfw3;
 import raylib;
-
+import std.stdio;
 
 void main(){
-	
+	validateRaylibBinding();
+	InitWindow(800, 600, "Edit Craft");
+	SetTargetFPS(60);
+	while (!WindowShouldClose()){
+		BeginDrawing();
+		ClearBackground(Colors.BLACK);
+		DrawText("Hello, User...", 400, 300, 28, Colors.RAYWHITE);
+		EndDrawing();
+	}
+	CloseWindow();
 }
