003a814a5664a43e645ebdde0248b03c8f15cc5a
itsjacob <conyersjacob21@gmail.com>
2024-11-09 12:00:00 -0600

    remove  python files

--- mobs/monsters/skeleton/Skeleton.py
+++
@@ -1,7 +0,0 @@
-from pyglet.gl import *
-#glEnable(texture.target)
-
-batch = pyglet.graphics.Batch()
-
-class skeleton():
-    torso = pyglet.model.load(r"C:\Users\SHANT\Desktop\f\programing\projects\EditCraft\mobs\monsters\skeleton\Skeleton_model\torso.obj", batch=batch)
