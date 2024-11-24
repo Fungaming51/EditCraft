61b89c1e3d5fa4c1f556d6dd3c946ccf3b485353
itsjacob <conyersjacob21@gmail.com>
2024-11-24 04:09:03 -0600

    Fix placeholder not loading
    extra underscore was added (I should go to sleep, it's 4 am right now)

--- source/app.d
+++ source/app.d
@@ -4,7 +4,7 @@ import std.stdio;
 void main(){
 	InitWindow(800, 600, "Edit Craft");
 	
-	Texture2D texture = LoadTexture("player__sprite.png");
+	Texture2D texture = LoadTexture("player_sprite.png");
 	
 	SetTargetFPS(60);
 	while (!WindowShouldClose()){
@@ -12,7 +12,7 @@ void main(){
 		ClearBackground(Colors.BLACK);
 		//DrawText("Hello, User...", 250, 250, 28, Colors.RAYWHITE);
 		
-		//DrawRectangle(100, 100, 100, 100, Colors.RED);
+		DrawRectangle(100, 100, 100, 100, Colors.RED);
 		
 		DrawTexture(texture, 100, 200, Colors.WHITE);
 		
