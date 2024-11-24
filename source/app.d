e720bb0fbde29332b0a85fd5d53fe3aaeb048cc1
itsjacob <conyersjacob21@gmail.com>
2024-11-24 04:05:43 -0600

    Add placeholder texture
    it won't load, but it's there...

--- source/app.d
+++ source/app.d
@@ -3,15 +3,23 @@ import std.stdio;
 
 void main(){
 	InitWindow(800, 600, "Edit Craft");
+	
+	Texture2D texture = LoadTexture("player__sprite.png");
+	
 	SetTargetFPS(60);
 	while (!WindowShouldClose()){
 		BeginDrawing();
 		ClearBackground(Colors.BLACK);
 		//DrawText("Hello, User...", 250, 250, 28, Colors.RAYWHITE);
 		
-		DrawRectangle(100, 100, 100, 100, Colors.RED);
+		//DrawRectangle(100, 100, 100, 100, Colors.RED);
+		
+		DrawTexture(texture, 100, 200, Colors.WHITE);
 		
 		EndDrawing();
 	}
+	
+	//UnloadTexture();
+	
 	CloseWindow();
 }
